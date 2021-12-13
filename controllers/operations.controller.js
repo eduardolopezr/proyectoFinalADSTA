3897dff9ca3d3a5e64711d93a119777c2915d759
eduardolora7 <jesuse_166@hotmail.com>
2021-12-13 16:38:38 -0600

    Agrega caracteristicas solicitadas al controlador shipment y operations

--- controllers/operations.controller.js
+++ controllers/operations.controller.js
@@ -1,17 +1,54 @@
 module.exports = {
     sum: function (req, res) {
-        res.status(201).send();
+
+        var num1 = Number.req.query.num1;
+        var num2 = Number.req.query.num2;
+
+        var result = num1+num2;
+
+        res.status(201).send({
+            operation: "Add",
+            result: result
+        });
     },
 
     substract: function (req, res) {
-        res.status(201).send();
+
+        var num1 = Number.req.query.num1;
+        var num2 = Number.req.query.num2;
+
+        var result = num1-num2;
+
+        res.status(201).send({
+            operation: "Substract",
+            result: result
+
+        });
     },
 
     multiply: function (req, res) {
-        res.status(201).send();
+        var num1 = Number.req.query.num1;
+        var num2 = Number.req.query.num2;
+
+        var result = num1*num2;
+
+        res.status(201).send({
+            operation: "Multiply",
+            result: result
+        });
     },
 
     divide: function (req, res) {
-        res.status(201).send();
+
+        var num1 = Number.req.query.num1;
+        var num2 = Number.req.query.num2;
+        var result;
+        
+        (num1>0) ? result = num1/num2 : result ="No se puede dividir entre 0";
+        
+        res.status(201).send({
+            operation: "Divide",
+            result: result
+        });
     }
 };
--- controllers/shipment.controller.js
+++ controllers/shipment.controller.js
@@ -1,13 +1,31 @@
 //deberas de utilizar faker para generar los datos
+const faker = require('faker');
 module.exports = {
     createShipment: function (req, res) {
         //debera de simular un envio con dirección un precio y una persona con sus datos
-        res.status(201).send();
+        var direccionPersona = faker.address.direction();
+        var precio = faker.commerce.price();
+        var nombrePersona = faker.name.findName();
+        
+        var response = {
+            message:"Pedido realizado...",
+            data:{
+                address: direccionPersona,
+                name: nombrePersona,
+                price: precio,
+            }
+        }
+        res.status(201).send(response);
     },
     changeStatus: function (req, res) {
         //Debera de retornar una dirección random
         // codigo de respuesta 201
         // data la direcciòn random
-        res.json({addres: ""});
+        var direccion = faker.address.direction();
+
+        res.status(201).json({
+            data: direccion
+        });
+        
     },
 };
