f0b137afa45d3192ff9f65231cb0c440a92ae48a
eduardolora7 <jesuse_166@hotmail.com>
2021-12-14 19:16:00 -0600

    Corrige los test. 12 test correctos

--- routes/payment.js
+++ routes/payment.js
@@ -4,9 +4,11 @@ var paymentController = require('../controllers/payment.controller');
 var middleware = require('../middleware');
 
 
-router.use(middleware);
+//router.use(middleware);
 
-router
-    .get('/promos', paymentController.getPromos);
+//Usamos las funciones que creamos en el controlador de payment.
+router.get('/promos', paymentController.getPromos);
+router.get('/create', paymentController.create);
+router.post('/applydiscount',paymentController.applyDiscount);
 
 module.exports = router;
